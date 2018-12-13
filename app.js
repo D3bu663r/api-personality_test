f962bb0353297ad4ac71d1c4b41e8cb52aba12d8
rafael <rafael.souza@idealeware.com.br>
2018-12-13 12:46:39 -0200

    adicionando cors

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const routers = require('./api/routers');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+
+app.use('*', cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 
 app.use('/', routers);
 
