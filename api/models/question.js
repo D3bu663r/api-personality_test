b5343b6e3550b6588a287936a6ebae83e2905888
Rafael <rafaelssouzaads@gmail.com>
2018-12-15 15:26:12 -0200

    refatoração

--- api/models/question.js
+++ api/models/question.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const Category = require('../models/category');
 const BadRequest = require('../errors/bad_request');
 
 const schema = new mongoose.Schema({
@@ -23,24 +22,6 @@ const schema = new mongoose.Schema({
         type: [String],
         required: true
     }
-});
-
-function existsCategory(question, next) {
-    Category.findOne({ name: question.category })
-        .then(function (category) {
-            if (category) return next();
-            next(new BadRequest('Categoria não cadastrada'));
-        }).catch(next);
-}
-
-schema.pre('save', function (next) {
-    if (!this.isModified('category')) return next();
-    existsCategory(this, next);
-});
-
-schema.pre('findOneAndUpdate', function (next) {
-    if (!this._update.category) return next();
-    existsCategory(this._update, next);
 });
 
 module.exports = mongoose.model('question', schema);
--- api/routers/index.js
+++ api/routers/index.js
@@ -3,7 +3,6 @@ const auth = require('../middlewares/auth');
 const util = require('../middlewares/utils');
 
 require('./auth')(router, auth, util);
-require('./category')(router, auth, util);
 require('./question')(router, auth, util);
 require('./user')(router, auth, util);
 require('./swagger')(router, auth, util);
